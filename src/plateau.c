464b582f22ffd247f038b061e851e60dc7e644d6
Nyakouai <asiatdragon@hotmail.fr>
2014-12-13 20:19:49 +0100

    Commit 2
    Still test + functions

--- src/plateau.c
+++ src/plateau.c
@@ -9,7 +9,18 @@
 struct sPlateau{
 
     Pions * pions;
-    Position ** position;
+    Position position[16][16];
     Joueur * joueur;
 
 };
+
+sPlateau initPlateau()
+{
+    unsigned int i, j;
+    sPlateau newPlateau;
+    for(i=0; i<16; i++)
+        for(j=0; j<16;j++)
+        {
+            newPlateau.position = initPosition(j, i);
+        }
+}
--- src/position.c
+++ src/position.c
@@ -2,11 +2,23 @@
 #include <stdlib.h>
 
 #include "position.h"
+#include "pion.h"
 
 struct sPosition{
 
     unsigned int x;
     unsigned int y;
+    Pion pPion;
+
 
 };
+
+sPosition initPosition(unsigned int abcisse, unsigned int ordonnee)
+{
+    sPosition newPosition;
+    newPosition.x = abcisse;
+    newPosition.y = ordonnee;
+    pPion = NULL;
+    return newPosition;
+}
 
